efcdd0d9bb26f19b1def6a768f5b96ce90eb7772
Sam Johnson <sjohnson540@gmail.com>
2016-12-26 17:40:37 -0800

    Add basic controls in raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,72 @@
 extern crate termion;
 
-use std::io::{self, Write};
+use std::io::{self, Write, Read};
 use termion::event::Key;
 use termion::{color, style, clear, cursor};
 use termion::raw::IntoRawMode;
+use termion::input::TermRead;
 
 fn main() {
     // Setup
     let mut stdout = io::stdout().into_raw_mode()
         .expect("Cannot get raw stdout");
+    let stdin = io::stdin();
 
     // Start Editing
     init(&mut stdout);
-    read_input(&mut stdout);
+    run(&mut stdout, stdin);
 }
 
 // Setup the way the editor looks
 fn init<W: Write>(out: &mut W) {
-    write!(out, "{}{}\n{}Realtime EDitor{}\n",
+    write!(out, "{}{}{}Realtime EDitor{}\n{}",
            clear::All,
            cursor::Goto(1,1),
            color::Fg(color::Red),
-           style::Reset).expect("Cannot open TTY");
+           style::Reset,
+           cursor::Goto(1,2))
+        .expect("Cannot open TTY");
     out.flush().expect("Cannot flush message");
 }
 
-// Continously read input from stdin (line-based)
-fn read_input<W: Write>(out: &mut W) {
-    let mut read = String::new();
-    let mut counter = 1;
-    loop {
-        let prompt = format!("{}: ", counter);
-        let line = get_line(&prompt.into_bytes(), &mut read, out);
-        let trimmed_line = line.trim();
+// Read characters as the user types them.
+// Quit on ESC
+fn run<W: Write, R: Read>(out: &mut W, input: R) {
+    let mut cx = 1;
+    let mut cy = 2;
 
-        if trimmed_line == "q" {
-            break;
-        } else if trimmed_line == "top" {
-            println!("{}", cursor::Goto(1,1));
+    for c in input.keys() {
+        write!(out, "{}", termion::cursor::Goto(cx, cy)).
+            expect("Could not move cursor");
+
+        match c.unwrap() {
+            Key::Char('q') => break,
+            Key::Char(c) => {
+                write!(out, "{}", c).unwrap();
+                cx += 1;
+            },
+            Key::Alt(c) => println!("^{}", c),
+            Key::Ctrl(c) => println!("*{}", c),
+            Key::Left => println!("←"),
+            Key::Right => println!("→"),
+            Key::Up => {
+                if cy > 1 {
+                    cy -= 1;
+                }
+            },
+            Key::Down => {
+                cy += 1;
+            },
+            Key::Backspace => println!("×"),
+            _ => {},
         }
-
-        counter += 1;
+        
+        out.flush().unwrap()
     }
-    println!("Read data:\n{}", read);
 }
 
 // Read a single line of input from a given prompt
+#[allow(dead_code)]
 fn get_line<W: Write>(prompt: &[u8], buf: &mut String, out: &mut W) -> String {
     // Prompt user
     out.write(prompt).expect("Could not write to Stdout");
